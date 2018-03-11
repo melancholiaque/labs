8303f552a4a93641524bd0b9f2c91ae34bd5d854
melancholaique <melancholique655312@gmail.com>
2018-03-11 13:41:18 +0200

    basic implementation completed

---
+++ parallel/first-lab/utility.py
@@ -0,0 +1,32 @@
+import numpy as np
+from settings import *
+import socket
+
+def gen_sample():
+    for FILE in (FNAME1, FNAME2):
+        with open(FILE,'w') as fp:
+            for i in tqdm(range(ROWS)):
+                rand_range = np.random.rand(COLS)
+                fp.write(f"{','.join(map(str, rand_range))}\n")
+                del rand_range
+
+def send(msg):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ADDR,PORT))
+    s.send(msg.encode() + b'\n')
+    s.close()
+
+def send_return(msg):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ADDR,PORT))
+    s.send(msg.encode() + b'\n')
+    result = ''
+    chunk = s.recv(1024)
+    while chunk:
+        if b'end' in chunk:
+            result += chunk.decode().rstrip('end')
+            s.close()
+            return result
+        else:
+            result += chunk.decode()
+        chunk = s.recv(4096)
